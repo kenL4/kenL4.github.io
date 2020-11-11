90b73877f2173518300deef6a0a197d826e38a7f
kenL4 <kenl2005@outlook.com>
2020-11-11 21:33:28 +0000

    v2.0.1

--- dog_api_guesser/sketch.js
+++ dog_api_guesser/sketch.js
@@ -1,47 +1,65 @@
 let classifier;
 let img;
-
-function modelReady() {
-	console.log("Model Ready");
-	classifier.classify(img, gotResults);
-}
+let parser;
+let message;
 
 function gotResults(error, data) {
+	console.log(message);
+	var correct = false;
+	var name = message.slice(30, -1).split("/")[0].split("-");
 	if (error) {
 		console.error(error);
 	} else {
 		console.log(data);
+		var guess = document.getElementById('ai_guesser');
+		guess.innerHTML = "I think it's a " + data[0].label;
+		formattedGuesses = data[0].label.split(", ");
+		console.log(formattedGuesses);
+		console.log(name.join(" "));
+		for (i = 0; i < formattedGuesses.length; i++){
+			guess = formattedGuesses[i].toLowerCase()
+			correctGuess = guess == name.join(" ") || guess == name.reverse().join(" ")
+			if (correctGuess) {
+				correct = true;
+			}
+		}
+		if (correct) {
+			win = document.getElementById('ai_rightorwrong');
+			win.innerHTML = "Correct";
+			win.style.color = "green";
+		} else {
+			win = document.getElementById('ai_rightorwrong');
+			win.innerHTML = "Wrong";
+			win.style.color = "red";
+		}
 	}
 }
 
 function imageReady() {
 	console.log("Image Ready");
 	image(img, 0, 0, width, height);
+	classifier.classify(img, gotResults);
 }
 
 function setup() {
 	var canvas = createCanvas(windowWidth/2, windowHeight/2);
-	canvas.position((windowWidth-width)/2, (windowHeight-height)/2);
-	img = loadImage(randomDoggo(), imageReady);
-	classifier = ml5.imageClassifier('MobileNet', modelReady);
+	canvas.position((windowWidth-width)/2, (windowHeight-height)/2); //centering it because css won't work
+	classifier = ml5.imageClassifier('MobileNet', randomDoggo);
 }
 
 function randomDoggo() {
 	//Ajax to load the dog-ceo-api
 	const HTTP = new XMLHttpRequest();
-	const url = "https://dog.ceo/api/breeds/image/random"
+	const url = "https://dog.ceo/api/breeds/image/random";
+	message = "https://images.dog.ceo/breeds/retriever-golden/n02099601_6980.jpg";
 	HTTP.open("GET", url);
 	HTTP.send();
-	HTTP.onreadystatechange= function() {
+	HTTP.onload = function() {
 		if (this.status == 200) {
 			data = HTTP.responseText;
 			data = JSON.parse(data);
-			message = data["message"];
-			return message.toString();
+			message = data.message
+			img = loadImage(message, imageReady);
 		}
 	}
-}
-function pred(){
-	img = loadImage(randomDoggo(), imageReady);
-	classifier.classify(img, gotResults);
 }
